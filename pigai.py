7d0c3ee374fc829e8220d4fbf233441304cec042
Bahtya <1532386727@qq.com>
2019-03-08 00:10:47 +0800

    V1.0.2

--- pigai.py
+++ pigai.py
@@ -1,7 +1,8 @@
 import requests
 
-login_url='http://open.pigai.org/?c=api&a=sentbase&action=login&key=SE9mpdwTPxyKGUvJb9vrVfYlwX803YcH&type=json&pigaimobile=1&pigaiversion=1.4.4&uid=13312451&token=600f8005baa69bc14a1e80de1664093c'
+base_url='http://open.pigai.org/'
 headers = {
+        'Connection':'keep-alive',
         'Content-Length': '326',
         'Accept': 'application/json',
         'Origin': 'file://',
@@ -12,7 +13,11 @@ headers = {
         'Accept-Language': 'zh-CN,en-US;q=0.9',
 }
 
-data = {'''
+session=requests.session()
+
+def login():
+    url = base_url + '?c=api&a=sentbase&action=login&key=SE9mpdwTPxyKGUvJb9vrVfYlwX803YcH&type=json&pigaimobile=1&pigaiversion=1.4.4&uid=13312451&token=600f8005baa69bc14a1e80de1664093c'
+    data = {'''
         'c':'api',
         'a':'sentbase',
         'action':'login',
@@ -38,19 +43,35 @@ data = {'''
         'MB_cid':'905ce29dbbc0ff14fab1ca7ea4469ab1',
         'MB_token':'905ce29dbbc0ff14fab1ca7ea4469ab1',
         'MB_wid':'org.pigai.allround'
-}
-
-urls = 'http://open.pigai.org/?c=api&a=essaylistbyuid'
-
-def login():
-    session=requests.session()
-    r=session.post(login_url,headers=headers,data=data)
-    result=r.json()
+    }
+    r = session.post(url,headers=headers,data=data)
+    result = r.json()
+    s = result['data']
+    print(s)
     if result['data'] !='':
         print('登陆成功')
-        
+        return(result['data']['minfo']['pigai_token'])
     else:
         print('登陆失败')
 
+def article_list(token):
+    url = base_url + '?c=api&a=essaylistbyuid&start=0&limit=10&pigaimobile=1&pigaiversion=1.4.4&uid=13312451&token=' + token
+    data = {
+        'abctest':'1',
+        'MB_time':'1551877327',
+        'MB_version':'1.4.4',
+        'MB_os[android]':'true',
+        'MB_os[version]':'7.1.1',
+        'MB_os[isBadAndroid]':'false',
+        'MB_os[plus]':'true',
+        'MB_uuid':'864166039376763,864166039376771',
+        'MB_cid':'905ce29dbbc0ff14fab1ca7ea4469ab1',
+        'MB_token':'905ce29dbbc0ff14fab1ca7ea4469ab1',
+        'MB_wid':'main'
+    }
+    r = session.post(url,headers = headers,data = data)
+    print(r.text)
+
 if __name__ == '__main__' :
-    login()
+    token = login()
+    article_list(token)
